1ebae9a490f3cedca7faadcfe3df757111699782
Tré Dudman <19359682+Tremus@users.noreply.github.com>
2023-11-20 23:53:23 +1000

    Fix build with MSC

--- sds.h
+++ sds.h
@@ -49,38 +49,50 @@ extern const char *SDS_NOINIT;
 typedef long long ssize_t;
 #endif
 
+#ifdef __GNUC__
+#define SDS_GNU_PACKED __attribute__ ((__packed__))
+#define SDS_MSC_PACK_START
+#define SDS_MSC_PACK_END
+#elif _MSC_VER
+#define SDS_GNU_PACKED 
+#define SDS_MSC_PACK_START __pragma(pack(push, 1) )
+#define SDS_MSC_PACK_END __pragma(pack(pop))
+#endif
+
 typedef char *sds;
 
 /* Note: sdshdr5 is never used, we just access the flags byte directly.
  * However is here to document the layout of type 5 SDS strings. */
-struct __attribute__ ((__packed__)) sdshdr5 {
+SDS_MSC_PACK_START
+struct SDS_GNU_PACKED sdshdr5 {
     unsigned char flags; /* 3 lsb of type, and 5 msb of string length */
     char buf[];
 };
-struct __attribute__ ((__packed__)) sdshdr8 {
+struct SDS_GNU_PACKED sdshdr8 {
     uint8_t len; /* used */
     uint8_t alloc; /* excluding the header and null terminator */
     unsigned char flags; /* 3 lsb of type, 5 unused bits */
     char buf[];
 };
-struct __attribute__ ((__packed__)) sdshdr16 {
+struct SDS_GNU_PACKED sdshdr16 {
     uint16_t len; /* used */
     uint16_t alloc; /* excluding the header and null terminator */
     unsigned char flags; /* 3 lsb of type, 5 unused bits */
     char buf[];
 };
-struct __attribute__ ((__packed__)) sdshdr32 {
+struct SDS_GNU_PACKED sdshdr32 {
     uint32_t len; /* used */
     uint32_t alloc; /* excluding the header and null terminator */
     unsigned char flags; /* 3 lsb of type, 5 unused bits */
     char buf[];
 };
-struct __attribute__ ((__packed__)) sdshdr64 {
+struct SDS_GNU_PACKED sdshdr64 {
     uint64_t len; /* used */
     uint64_t alloc; /* excluding the header and null terminator */
     unsigned char flags; /* 3 lsb of type, 5 unused bits */
     char buf[];
 };
+SDS_MSC_PACK_END
 
 #define SDS_TYPE_5  0
 #define SDS_TYPE_8  1
